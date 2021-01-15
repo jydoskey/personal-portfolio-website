a5612a7c1f6f3a969b71431fa2f38122ffec26b7
jide_omotola <jide@reach.africa>
2021-01-15 12:15:12 +0100

    request form created

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,20 +5,38 @@
         <div slot="card-body" class="card-body">
           <div class="card-body__content">
             <div class="container">
-              <div class="row align-items-center">
+              <div class="row align-items-center justify-content-between">
                 <div class="col-lg-6">
                   <div class="text-center card-header__placeholder">
                     <h2 class="card-header pt-0 pb-4">{Contact Me}</h2>
                   </div>
                 </div>
-                <div class="col-lg-6 mb-3">
+                <div class="col-lg-5 mb-3">
                   <Card class="contact-html">
                     <div slot="card-header" class="card-header text-center card-header__placeholder">
                       <p class="p-1 caption">Hire Me For Your Projects</p>
                     </div>
                     <div slot="card-body" class="card-body pt-3">
                       <div class="card-body__content pt-0">
-                        
+                        <form action="">
+                          <label>
+                            <span>Full Name</span>
+                            <input type="text" />
+                          </label>
+                          <label>
+                            <span>Email</span>
+                            <input type="email" />
+                          </label>
+                          <label>
+                            <span>Phone</span>
+                            <input type="number" />
+                          </label>
+                          <label>
+                            <span>Message</span>
+                            <input type="text" />
+                          </label>
+                          <button class="btn btn-magenta btn-send btn-sm ml-auto mr-auto mt-5">Send Message</button>
+                        </form>
                       </div>
                     </div>
                   </Card>
@@ -67,11 +85,65 @@
 
   .contact-html {
     background: #ffffff;
-    border-radius: 0.2rem;
+    border-radius: 0.25rem;
     color: #303030;
 
     p.caption {
       color: #303030;
     }
+  }
+
+  input {
+    border: none;
+    outline: none;
+    background: none;
+  }
+
+  label {
+    display: block;
+    margin: 25px auto 0;
+    text-align: start;
+
+    span {
+      font-size: 14px;
+      color: #828282;
+    }
+  }
+
+  input {
+    display: block;
+    width: 100%;
+    margin-top: 5px;
+    padding-bottom: 5px;
+    font-size: 15px;
+    color: #303030;
+    border-bottom: 1px solid $magenta;
+    text-align: start;
+  }
+
+  .btn-send {
+    width: 10rem;
+    height: 2.9375rem;
+    padding-left: 0;
+    padding-right: 0;
+    background: #009D9D;
+    color: white;
+    display: block;
+
+    &:hover {
+      background: #008080;
+    }
+  }
+
+  /* Chrome, Safari, Edge, Opera */
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  input[type=number] {
+    -moz-appearance: textfield;
   }
 </style>
