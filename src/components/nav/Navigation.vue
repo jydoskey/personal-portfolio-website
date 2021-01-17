aadee67aa4aaa7aa3ae7948deecfcd82774c6880
jide_omotola <jide@reach.africa>
2021-01-17 22:01:06 +0100

    home nav link set to reload

--- src/components/nav/Navigation.vue
+++ src/components/nav/Navigation.vue
@@ -24,7 +24,7 @@
       return {
         mainNavLinks: [{
             name: 'Home',
-            url: '#landing'
+            url: '/'
           }, {
             name: 'Projects',
             url: '#projects'
