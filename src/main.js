949b0adbfb8e10424a984382a01674f7f6609fe6
jide_omotola <jide@reach.africa>
2021-01-22 23:06:25 +0100

    added package for ease in view

--- src/main.js
+++ src/main.js
@@ -5,10 +5,16 @@ import router from "@/router/router";
 import moment from 'moment';
 Vue.prototype.moment = moment
 
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  created () {
+    AOS.init()
+},
   router,
   render: h => h(App),
 }).$mount('#app')
