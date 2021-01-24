ef13a5c067a7935d59dda9249b9f0875bb30e4f9
jide_omotola <jide@reach.africa>
2021-01-24 03:18:22 +0100

    removed transition package

--- src/main.js
+++ src/main.js
@@ -5,9 +5,6 @@ import router from "@/router/router";
 import moment from 'moment';
 Vue.prototype.moment = moment
 
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-
 import { KinesisContainer, KinesisElement} from "vue-kinesis"
 
 Vue.component('kinesis-container', KinesisContainer)
@@ -17,9 +14,6 @@ Vue.component('kinesis-element', KinesisElement)
 Vue.config.productionTip = false
 
 new Vue({
-  created () {
-    AOS.init()
-},
   router,
   render: h => h(App),
 }).$mount('#app')
