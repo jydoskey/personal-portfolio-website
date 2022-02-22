42308bd81b9d13c4bd1d3f370028c37ee4a80b08
jydoskey <jideomotola33@gmail.com>
2022-02-22 12:47:08 +0100

    made adjustement to app file

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,22 @@
 
 export const routes = [{
     path: '/',
-    component: () => import("@/components/LandingPage")
-  },
-  {
-    path: '/projects',
-    component: () => import ("@/components/Work")
-  },
-  {
-    path: '/about',
-    component: () => import ("@/components/About")
-  },
-  {
-    path: '/contact',
-    component: () => import ("@/components/Contact")
+    component: () => import("@/components/layout/ComingSoon")
   }
+  //   path: '/',
+  //   component: () => import("@/components/LandingPage")
+  // },
+  // {
+  //   path: '/projects',
+  //   component: () => import ("@/components/Work")
+  // },
+  // {
+  //   path: '/about',
+  //   component: () => import ("@/components/About")
+  // },
+  // {
+  //   path: '/contact',
+  //   component: () => import ("@/components/Contact")
+  // }
   
 ];
